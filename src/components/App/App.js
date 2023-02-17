92f812fe0f3a4e2f77aa959c20cb9ba25ddb27fe
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-17 15:58:39 -0800

    afternoon tweaks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import defaultClothingItems from '../../utils/defaultClothingItems.js';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
+import WeatherCard from '../WeatherCard/WeatherCard';
 
 function App() {
   console.log(defaultClothingItems);
