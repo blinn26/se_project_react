0221f8bed0f41bd9552ade590e572f7c692fe94d
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-18 09:55:03 -0800

    morning tweaks

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,38 +3,35 @@ import ItemCard from '../ItemCard/ItemCard';
 import WeatherCard from '../WeatherCard/WeatherCard';
 import CurrentTemperatureUnitContext from '../../contexts/CurrentTemperatureUnitContext';
 import './Main.css';
+import defaultClothingItems from '../../utils/defaultClothingItems';
 
-const Main = ({ cards, onCardClick, weatherType, defaultClothingItems, onItemClick }) => {
+function Main({ defaultClothingItems, onItemClick }) {
   const { currentTemperatureUnit, weatherData } = useContext(CurrentTemperatureUnitContext);
 
   function cardFilter() {
-    return cards
-      .filter((card) => card.weather === weatherType)
-      .map((filteredCard) => <ItemCard key={filteredCard.id} card={filteredCard} onCardClick={onCardClick} />);
+    return defaultClothingItems
+      .filter((item) => item.weather === weatherData.type)
+      .map((filteredItem) => <ItemCard key={filteredItem._id} item={filteredItem} onItemClick={onItemClick} />);
   }
 
   return (
-    <Main className='main'>
+    <main className='main'>
       <WeatherCard />
       <section className='main__clothes'>
         <div className='main__info'>
           <div className='main__description-container'>
             <p className='main__description'>
               Today is {weatherData.temperature && weatherData.temperature[currentTemperatureUnit]} and it is{' '}
-              {weatherType}
+              {weatherData.type}
             </p>
             <p className='main__description'>&nbsp;/&nbsp;</p>
             <p className='main__description'>You may want to wear:</p>
           </div>
         </div>
-        <ul className='main__items'>
-          {cards
-            ? cardFilter()
-            : defaultClothingItems.map((item) => <ItemCard item={item} key={item._id} onItemClick={onItemClick} />)}
-        </ul>
+        <ul className='main__items'>{defaultClothingItems ? cardFilter() : null}</ul>
       </section>
-    </Main>
+    </main>
   );
-};
+}
 
 export default Main;
