5ba101a971e9cc53e92dcd42491fbf614e9dc3dc
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-19 15:13:38 -0800

    app half way

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,48 @@
 import React from 'react';
-import './App.css';
-import defaultClothingItems from '../../utils/defaultClothingItems.js';
-import Header from '../Header/Header';
+import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import { defaultClothingItems } from '../../utils/defaultClothingItems.js';
 import ItemCard from '../ItemCard/ItemCard';
+import { location } from '../../utils/constants';
+import { getForecastWeather, filterDataFromWeatherApi } from '../../utils/weatherApi';
 
-function App() {
-  console.log(defaultClothingItems);
-  console.log(defaultClothingItems.length);
-  const onItemClick = (card) => {
-    console.log(card);
+const App = () => {
+  const [wetherData, setWetherData] = React.useState({});
+  const [clothingItems, setClothingItems] = React.useState({});
+  const [activeModal, setActiveModal] = React.useState();
+  const [selectCard, setSelectCard] = React.useState(null);
+
+  const handleCardClick = (card) => {
+    setActiveModal('preview');
+    setSelectCard(card);
   };
 
-  return (
-    <div className='App'>
-      <Header />
-      <main className='main'>
-        <div>
-          {defaultClothingItems.map((item) => (
-            <ItemCard item={item} key={item._id} onItemClick={onItemClick} />
-          ))}
-        </div>
-      </main>
+  const closeAllModals = () => {
+    setActiveModal();
+  };
 
-      <Footer className='footer'>This is the footer</Footer>
+  React.useEffect(() => {
+    if (location.latitude && location.longitude) {
+      // app key
+      getForecastWeather(location.latitude, location.longitude)
+        .then((data) => {
+          setWetherData(filterDataFromWeatherApi(data));
+        })
+        .catch((err) => console.log(err));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    setClothingItems(defaultClothingItems);
+  }, []);
+
+  return (
+    <div className='page'>
+      <div clasName='page__wrapper'>
+        <Header wetherData={wetherData} handleCardClick={() => setActiveModal('create')} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,14 +1,13 @@
-const getForecastWeather = (location, APIkey) => {
+const getForecastWeather = async (location, APIkey) => {
   const parsedLocation = `${location.latitude},{location.longitude}`;
-  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${parsedLocation}&appid=${APIkey}&units=metric`).then(
-    (res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        return Promise.reject(`Error: ${res.status}`);
-      }
-    }
+  const res = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8bec312d909f4c92c9729d7ff0366d1f=${parsedLocation}&appid=${APIkey}&units=metric`
   );
+  if (res.ok) {
+    return res.json();
+  } else {
+    return Promise.reject(`Error: ${res.status}`);
+  }
 };
 
 const filterDataFromWeatherApi = (data) => {
