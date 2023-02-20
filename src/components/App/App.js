3e6bdfee7b6bc178e138a1224e9c8c5da5b9466b
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-19 16:17:40 -0800

    project on react

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import { location } from '../../utils/constants';
 import { getForecastWeather, filterDataFromWeatherApi } from '../../utils/weatherApi';
 
 const App = () => {
-  const [wetherData, setWetherData] = React.useState({});
+  const [weatherData, setWeatherData] = React.useState({});
   const [clothingItems, setClothingItems] = React.useState({});
   const [activeModal, setActiveModal] = React.useState();
   const [selectCard, setSelectCard] = React.useState(null);
@@ -26,7 +26,7 @@ const App = () => {
       // app key
       getForecastWeather(location.latitude, location.longitude)
         .then((data) => {
-          setWetherData(filterDataFromWeatherApi(data));
+          setWeatherData(filterDataFromWeatherApi(data));
         })
         .catch((err) => console.log(err));
     }
@@ -39,10 +39,60 @@ const App = () => {
   return (
     <div className='page'>
       <div clasName='page__wrapper'>
-        <Header wetherData={wetherData} handleCardClick={() => setActiveModal('create')} />
+        <Header weatherData={weatherData} handleCardClick={() => setActiveModal('create')} />
+        <Main weatherData={weatherData} cards={clothingItems} onCardClick={handleCardClick} />
+        <Footer />
       </div>
+      {activeModal === 'create' && (
+        <ModalWithForm title='New garment' name='new-card' onClose={closeAllModals}>
+          <label className='modal__label'>
+            <input
+              type='text'
+              name='name'
+              id='place-name'
+              className='modal__input modal__input_type_card-name'
+              placeholder='title'
+              required
+              minLength='1'
+              maxLength='30'
+            />
+            <span className='modal__error' id='place-name-error'></span>
+          </label>
+          <label className='modal__label'>
+            <input
+              type='url'
+              name='link'
+              id='place-link'
+              className='modal__input modal__input_type_url'
+              placeholder='Image URL'
+              required
+            />
+            <span className='modal__error' id='place-link-error'></span>
+          </label>
+          <p> Select the weather type:</p>
+          <div className='modal__input modal__input_type_radio'>
+            <input type='radio' id='choicHot' name='weatherType' value='Hot' />
+            <label className='modal__label_radio' htmlFor='choicHot'>
+              Hot
+            </label>
+          </div>
+          <div>
+            <input type='radio' id='choiceWarm' name='weatherType' value='warm' />
+            <label className='modal__label_radio' htmlFor='choiceWarm'>
+              Warm
+            </label>
+          </div>
+          <div>
+            <input type='radio' id='choiceCold' name='weatherType' value='cold' />
+            <label className='modal__label_radio' htmlFor='choiceCold'>
+              Cold
+            </label>
+          </div>
+        </ModalWithForm>
+      )}
+
+      {activeModal === 'preview' && <ItemModal card={selectCard} onClose={closeAllModals} />}
     </div>
   );
 };
-
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,3 @@
-/* import React from 'react';
-import ItemCard from '../ItemCard/ItemCard';
-import './Main.css';
-import Header from '../Header/Header';
-
-const Main = ({ defaultClothingItems, onItemClick }) => {
-  return (
-    <Main class='main'>
-      {defaultClothingItems.map((item) => (
-        <ItemCard item={item} key={item._id} onItemClick={onItemClick} />
-      ))}
-    </Main>
-  );
-};
-
-export default Main; */
 import React from 'react';
 import ItemCard from '../ItemCard/ItemCard';
 import './Main.css';
