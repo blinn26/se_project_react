661a238771d8b8cdcd15d703c549267c6789f409
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-20 11:35:32 -0800

    weatherCard.js adding from clone ben

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,5 +1,16 @@
-const WeatherCard = () => {
-  return <div>WeatherCard</div>;
+import React from 'react';
+import './WeatherCard.css';
+
+const WeatherCard = ({ weatherData }) => {
+  const { temperature, description, icon } = weatherData;
+
+  return (
+    <div className='weather-card'>
+      <div className='weather-card__temp'>{temperature}°F</div>
+      <div className='weather-card__desc'>{description}</div>
+      <img className='weather-card__icon' src={icon} alt={description} />
+    </div>
+  );
 };
 
 export default WeatherCard;
