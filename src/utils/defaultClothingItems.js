1bc4065850082cef799c0cad9bf48f3122448517
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-19 16:42:54 -0800

    default clothing

--- src/utils/defaultClothingItems.js
+++ src/utils/defaultClothingItems.js
@@ -36,4 +36,4 @@ const defaultClothingItems = [
     link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4',
   },
 ];
-export default defaultClothingItems;
+export { defaultClothingItems };
