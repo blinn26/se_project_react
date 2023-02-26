22231216652e91dd7764578466f509ebb8392e43
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-25 18:34:46 -0800

    updated footer on project

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './Footer.css';
 
 export function Footer() {
   console.log('Footer');
 
   return (
     <footer className='footer'>
-      <p className='footer__name'>CREATEDbyBENLINN</p>
-      <p className='footer__name'>2023</p>
+      <div className='footer__container'>
+        <p className='footer__title'>CREATEDbyBENLINN</p>
+        <p className='footer__year'>2023</p>
+      </div>
     </footer>
   );
 }
