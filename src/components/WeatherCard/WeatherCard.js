dd4d9a8cb3263485808facbc409a2561ba07c71e
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-26 11:26:49 -0800

    updates morning

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -4,8 +4,15 @@ import './WeatherCard.css';
 const WeatherCard = ({ weatherData }) => {
   const { temperature, description, icon } = weatherData;
 
+  // className for weather=card will describe the background proportions, positions, whatever but not image url
+
+  const selectBackground = () => {
+    // if statements
+    // return image url
+  };
+
   return (
-    <div className='weather-card'>
+    <div className='weather-card' style={{ backgroundImage: selectBackground() }}>
       <div className='weather-card__temp'>{temperature}0°F</div>
       <div className='weather-card__desc'>{description}</div>
       <img className='weather-card__icon' src={icon} alt={description} />
