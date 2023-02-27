7a06772e3d5ea89c00d24504fdbf1d72884a1a6d
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-02-26 16:32:57 -0800

    weathercard js saves

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,23 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './WeatherCard.css';
+import { getForecastWeather, filterDataFromWeatherApi } from '../../utils/weatherApi';
 
-const WeatherCard = ({ weatherData }) => {
-  const { temperature, description, icon } = weatherData;
+const weatherImages = [
+  {
+    condition: 'Clear',
+    isDay: true,
+    image: '',
+  },
+  {
+    condition: 'Clear',
+    isDay: false,
+    image: 'path/to/clear-night-image.jpg',
+  },
+  // Add more objects for other weather conditions
+];
 
-  // className for weather=card will describe the background proportions, positions, whatever but not image url
+function WeatherCard({ deg, unit }) {
+  const [weatherData, setWeatherData] = useState(null);
+  const [backImage, setBackImage] = useState(null);
+  const [backColor, setBackColor] = useState(null);
+  const [backImageObject, setBackImageObject] = useState(null);
 
-  const selectBackground = () => {
-    // if statements
-    // return image url
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getForecastWeather();
+        const filteredData = filterDataFromWeatherApi(data);
+        setWeatherData(filteredData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (weatherData) {
+      setBackColor(weatherData.isDay ? 'rgba(0, 163, 255, 1)' : 'rgba(40, 104, 151, 1)');
+      setBackImageObject(
+        weatherImages.find((item) => {
+          return item.condition === weatherData.condition && item.isDay === weatherData.isDay;
+        })
+      );
+    }
+  }, [weatherData]);
+
+  useEffect(() => {
+    if (backImageObject) {
+      setBackImage(backImageObject.image);
+    }
+  }, [backImageObject]);
 
   return (
-    <div className='weather-card' style={{ backgroundImage: selectBackground() }}>
-      <div className='weather-card__temp'>{temperature}0°F</div>
-      <div className='weather-card__desc'>{description}</div>
-      <img className='weather-card__icon' src={icon} alt={description} />
+    <div
+      className='weather'
+      style={{
+        backgroundColor: backColor,
+        backgroundImage: `url(${backImage})`,
+      }}>
+      {weatherData && (
+        <p className='weather__temp'>
+          {weatherData.tempature}
+          {deg}°{unit}
+        </p>
+      )}
+      <p></p>
     </div>
   );
-};
+}
 
 export default WeatherCard;
