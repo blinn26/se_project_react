fd979faee32ee6be0d526682df75d40b2196d185
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-03-01 14:41:18 -0800

    cleaning

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,28 +3,32 @@ import '../Header/Header.css';
 import '../Header/Navigation.css';
 
 const Header = ({ weatherData, handleAddClick }) => {
-  if (!weatherData) return null;
-  const currentDate = new Date().toLocaleDateString('default', { month: 'long', day: 'numeric', year: 'numeric' });
-  const username = '';
+  // Destructure props
+  const { city } = weatherData || {};
+
+  // Get username from URL params
+  const params = new URLSearchParams(window.location.search);
+  const username = params.get('username');
 
   return (
-    <header className='header'>
-      <div className='header__container'>
-        <img src='/images/wtwr.svg' alt='wtwr logo' className='header__logo' />
-        <p className='header__date'>
-          {currentDate}, {weatherData.city}
-        </p>
-      </div>{' '}
-      <div className='header__nav'>
-        {username}
+    city && (
+      <header className='header'>
+        <div className='header__container'>
+          <img src='/images/wtwr.svg' alt='wtwr logo' className='header__logo' />
+          <p className='header__date'>
+            {`${new Date().toLocaleDateString('default', { month: 'long', day: 'numeric', year: 'numeric' })}, ${city}`}
+          </p>
+        </div>
 
-        <button onClick={handleAddClick} className='navigation__button '>
-          + Add clothes
-        </button>
-
-        <img className='navigation__user' src='/images/Avatar.svg' alt='user avatar default' />
-      </div>
-    </header>
+        <div className='header__nav'>
+          <span className='navigation__username'>{username || 'Ben'}</span>
+          <button onClick={handleAddClick} className='navigation__button'>
+            + Add clothes
+          </button>
+          <img className='navigation__user' src='/images/Avatar.svg' alt='user avatar default' />
+        </div>
+      </header>
+    )
   );
 };
 
