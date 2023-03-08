fe7f2f082a5cfa390f54756581e335bfac48925a
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-03-08 15:07:45 -0800

    save the fixed code

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -12,8 +12,8 @@ const ToggleSwitch = ({ isChecked, onToggle }) => {
         id='toggleSwitch'
       />
       <label className='toggle-switch-label' htmlFor='toggleSwitch'>
-        <span className='toggle-switch-inner' />
-        <span className='toggle-switch-switch' />
+        {/* <span className='toggle-switch-inner' /> */}
+        <span className='toggle-switch-switch toggle-switch-inner' />
       </label>
     </div>
   );
