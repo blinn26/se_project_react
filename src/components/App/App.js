1dc9b592d56ffd21fd78e84f4a6a6156d3d3420f
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-03-11 10:21:05 -0800

    app js for toggle switch weather

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,9 +7,8 @@ import ModalWithForm from '../ModalWithForm/ModalWithForm';
 import ItemModal from '../ItemModal/ItemModal';
 import { location } from '../../utils/constants';
 import { getForecastWeather, filterDataFromWeatherApi } from '../../utils/weatherApi';
-import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
 import './App.css';
-import currentTemperatureUnit from '../../context/currentTemperatureUnit';
+import { CurrentTemperatureUnitContext } from '../../context/currentTemperatureUnit';
 
 const APIKey = process.env.REACT_APP_WEATHER_API_KEY;
 
@@ -53,68 +52,74 @@ const App = () => {
   }, []);
 
   return (
-    <div className='page'>
-      <div className='page__wrapper'>
-        <Header weatherData={weatherData} handleAddClick={() => setActiveModal('create')} />
+    <CurrentTemperatureUnitContext.Provider
+      value={{
+        currentTemperatureUnit,
+        handleToggleSwitchChange: handleToggleSwitch,
+      }}>
+      <div className='page'>
+        <div className='page__wrapper'>
+          <Header weatherData={weatherData} handleAddClick={() => setActiveModal('create')} />
+          <Main weatherData={weatherData} cards={clothingItems} onCardClick={handleAddClick} />
+          <Footer />
+        </div>
+        {activeModal === 'create' && (
+          <ModalWithForm
+            name='new-card'
+            buttonText='Add garment'
+            title='New garment'
+            onSubmit={handleSubmit}
+            onClose={closeAllModals}>
+            <label className='modal__label'>Name</label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              className='modal__input modal__input_type-name'
+              placeholder='Name'
+              required
+              minLength='1'
+              maxLength='30'
+            />
+            <span className='modal__error' id='place-name-error'></span>
 
-        <Main weatherData={weatherData} cards={clothingItems} onCardClick={handleAddClick} />
-        <Footer />
+            <label className='modal__label'>Link</label>
+            <input
+              type='url'
+              name='link'
+              id='link'
+              className='modal__input modal__input_type-url'
+              placeholder='Image URL'
+              required
+            />
+            <span className='modal__error' id='place-link-error'></span>
+
+            <p> Select the weather type:</p>
+            <div className='modal__input modal__input_type_radio'>
+              <input type='radio' id='choicHot' name='weatherType' value='Hot' />
+              <label className='modal__label_radio' htmlFor='choicHot'>
+                Hot
+              </label>
+            </div>
+            <div>
+              <input type='radio' id='choiceWarm' name='weatherType' value='warm' />
+              <label className='modal__label_radio' htmlFor='choiceWarm'>
+                Warm
+              </label>
+            </div>
+            <div>
+              <input type='radio' id='choiceCold' name='weatherType' value='cold' />
+              <label className='modal__label_radio' htmlFor='choiceCold'>
+                Cold
+              </label>
+            </div>
+          </ModalWithForm>
+        )}
+
+        {activeModal === 'preview' && <ItemModal card={selectCard} onClose={closeAllModals} />}
       </div>
-      {activeModal === 'create' && (
-        <ModalWithForm
-          name='new-card'
-          buttonText='Add garment'
-          title='New garment'
-          onSubmit={handleSubmit}
-          onClose={closeAllModals}>
-          <label className='modal__label'>Name</label>
-          <input
-            type='text'
-            name='name'
-            id='name'
-            className='modal__input modal__input_type-name'
-            placeholder='Name'
-            required
-            minLength='1'
-            maxLength='30'
-          />
-          <span className='modal__error' id='place-name-error'></span>
-
-          <label className='modal__label'>Link</label>
-          <input
-            type='url'
-            name='link'
-            id='link'
-            className='modal__input modal__input_type-url'
-            placeholder='Image URL'
-            required
-          />
-          <span className='modal__error' id='place-link-error'></span>
-
-          <p> Select the weather type:</p>
-          <div className='modal__input modal__input_type_radio'>
-            <input type='radio' id='choicHot' name='weatherType' value='Hot' />
-            <label className='modal__label_radio' htmlFor='choicHot'>
-              Hot
-            </label>
-          </div>
-          <div>
-            <input type='radio' id='choiceWarm' name='weatherType' value='warm' />
-            <label className='modal__label_radio' htmlFor='choiceWarm'>
-              Warm
-            </label>
-          </div>
-          <div>
-            <input type='radio' id='choiceCold' name='weatherType' value='cold' />
-            <label className='modal__label_radio' htmlFor='choiceCold'>
-              Cold
-            </label>
-          </div>
-        </ModalWithForm>
-      )}
-
-      {activeModal === 'preview' && <ItemModal card={selectCard} onClose={closeAllModals} />}
-    </div>
+    </CurrentTemperatureUnitContext.Provider>
   );
 };
+
 export default App;
--- src/context/currentTemperatureUnit.js
+++ src/context/currentTemperatureUnit.js
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 
-const CurrentTemperatureUnitContext = createContext();
-
-export default CurrentTemperatureUnitContext;
+export const CurrentTemperatureUnitContext = createContext({
+  currentTemperatureUnit: 'F',
+  handleToggleSwitchChange: () => {},
+});
