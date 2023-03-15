6e2c794574e7e46c5f0f29959584ca0be511b7ec
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-03-15 09:54:59 -0700

    addItemModalJs

--- src/components/AddItemModal/AddItemModal.js
+++ src/components/AddItemModal/AddItemModal.js
@@ -5,12 +5,16 @@ const AddItemModal = ({ isOpen, onAddItem, onClose }) => {
   const [itemName, setItemName] = useState('');
   const [itemLink, setItemLink] = useState('');
   const [weatherType, setWeatherType] = useState('');
+  // onAddItem refers to handleAddItemSubmit, which is declared in App.js
+  // declare state for each input field
 
   useEffect(() => {
     setItemName('');
     setItemLink('');
     setWeatherType('');
   }, [isOpen]);
+  // use a useEffect hook to reset the input field state to empty strings when
+  // the modal is opened
 
   function handleNameChange(event) {
     setItemName(event.target.value);
@@ -29,11 +33,10 @@ const AddItemModal = ({ isOpen, onAddItem, onClose }) => {
     onAddItem(itemName, itemLink, weatherType);
   }
 
-  function handleRadioChange(event) {
-    setWeatherType(event.target.value);
-  }
-
+  // prevent default behavior
+  // call onAddItem with appropriate arguments
   return (
+    /* don't forget to pass appropriate props to ModalWithForm */
     <ModalWithForm
       card='new-card'
       buttonText='Add garment'
@@ -54,7 +57,6 @@ const AddItemModal = ({ isOpen, onAddItem, onClose }) => {
         value={itemName}
         onChange={handleNameChange}
       />
-
       <input
         type='url'
         name='link'
@@ -65,9 +67,8 @@ const AddItemModal = ({ isOpen, onAddItem, onClose }) => {
         value={itemLink}
         onChange={handleImageChange}
       />
-
       <span className='modal__error' id='place-link-error'></span>
-
+      /* the contents of the form will go in here */
       <p>Select the weather type:</p>
       <div className='modal__input modal__input_type_radio'>
         <input
