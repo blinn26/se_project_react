6d26ac1cdeace804783bbbd92163e6ca42ebeb20
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-03-15 09:51:01 -0700

    fixed the change use state prevent and all the fun stuff

--- src/components/AddItemModal/AddItemModal.js
+++ src/components/AddItemModal/AddItemModal.js
@@ -1,104 +1,114 @@
 import React, { useState, useEffect } from 'react';
 import ModalWithForm from '../ModalWithForm/ModalWithForm';
 
-// onAddItem refers to handleAddItemSubmit, which is declared in App.js
 const AddItemModal = ({ isOpen, onAddItem, onClose }) => {
-  const [ itemName, setItemName ] = useState('');
-  const [ itemLink, setItemLink ] = useState('');
-  const [ weatherType, setWeatherType ] = useState('');
-  // declare state for each input field
+  const [itemName, setItemName] = useState('');
+  const [itemLink, setItemLink] = useState('');
+  const [weatherType, setWeatherType] = useState('');
 
-  // use a useEffect hook to reset the input field state to empty strings when 
-  // the modal is opened
-  useEffect(() => { 
+  useEffect(() => {
     setItemName('');
     setItemLink('');
     setWeatherType('');
-  }, [ isOpen ]);
-  // create onChange handlers corresponding to each state variable
-
+  }, [isOpen]);
 
   function handleNameChange(event) {
     setItemName(event.target.value);
   }
 
-
- function handleImageChange(event) {
-  setItemLink(event.target.value);
-}
-
-
-  function handleWeatherTypeChange(event) {
+  function handleImageChange(event) {
     setItemLink(event.target.value);
   }
 
- 
-    // prevent default behavior
-    // call onAddItem with appropriate arguments
-  
+  function handleWeatherTypeChange(event) {
+    setWeatherType(event.target.value);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     onAddItem(itemName, itemLink, weatherType);
   }
-    // prevent default behavior
-    // call onAddItem with appropriate arguments
-  
+
+  function handleRadioChange(event) {
+    setWeatherType(event.target.value);
+  }
 
   return (
-    {/* don't forget to pass appropriate props to ModalWithForm */}
-         <ModalWithForm
-            card='new-card'
-            buttonText = 'Add garment'
-            isOpen = { isOpen }
-            onAddItem = { onAddItem }
-            title='New garment'
-            onSubmit={handleSubmit}
-            onClose={onClose}>
-            <label className='modal__label'>Name</label>
-            <input
-              type='text'
-              name='name'
-              id='name'
-              className='modal__input modal__input_type-name'
-              placeholder='Name'
-              required
-              minLength='1'
-              maxLength='30'
-            />
-            <span className='modal__error' id='place-name-error'></span>
+    <ModalWithForm
+      card='new-card'
+      buttonText='Add garment'
+      isOpen={isOpen}
+      title='New garment'
+      onSubmit={handleSubmit}
+      onClose={onClose}>
+      <label className='modal__label'>Name</label>
+      <input
+        type='text'
+        name='name'
+        id='name'
+        className='modal__input modal__input_type-name'
+        placeholder='Name'
+        required
+        minLength='1'
+        maxLength='30'
+        value={itemName}
+        onChange={handleNameChange}
+      />
 
-            <label className='modal__label'>Link</label>
-            <input
-              type='url'
-              name='link'
-              id='link'
-              className='modal__input modal__input_type-url'
-              placeholder='Image URL'
-              required
-            />
-            <span className='modal__error' id='place-link-error'></span>
+      <input
+        type='url'
+        name='link'
+        id='link'
+        className='modal__input modal__input_type-url'
+        placeholder='Image URL'
+        required
+        value={itemLink}
+        onChange={handleImageChange}
+      />
 
-            <p> Select the weather type:</p>
-            <div className='modal__input modal__input_type_radio'>
-              <input type='radio' id='choicHot' name='weatherType' value='Hot' />
-              <label className='modal__label_radio' htmlFor='choicHot'>
-                Hot
-              </label>
-            </div>
-            <div>
-              <input type='radio' id='choiceWarm' name='weatherType' value='warm' />
-              <label className='modal__label_radio' htmlFor='choiceWarm'>
-                Warm
-              </label>
-            </div>
-            <div>
-              <input type='radio' id='choiceCold' name='weatherType' value='cold' />
-              <label className='modal__label_radio' htmlFor='choiceCold'>
-                Cold
-              </label>
-            </div>
-          </ModalWithForm>
+      <span className='modal__error' id='place-link-error'></span>
+
+      <p>Select the weather type:</p>
+      <div className='modal__input modal__input_type_radio'>
+        <input
+          type='radio'
+          id='choiceHot'
+          name='weatherType'
+          value='Hot'
+          checked={weatherType === 'Hot'}
+          onChange={handleWeatherTypeChange}
+        />
+        <label className='modal__label_radio' htmlFor='choiceHot'>
+          Hot
+        </label>
+      </div>
+      <div>
+        <input
+          type='radio'
+          id='choiceWarm'
+          name='weatherType'
+          value='warm'
+          checked={weatherType === 'warm'}
+          onChange={handleRadioChange}
+        />
+        <label className='modal__label_radio' htmlFor='choiceWarm'>
+          Warm
+        </label>
+      </div>
+      <div>
+        <input
+          type='radio'
+          id='choiceCold'
+          name='weatherType'
+          value='cold'
+          checked={weatherType === 'cold'}
+          onChange={handleRadioChange}
+        />
+        <label className='modal__label_radio' htmlFor='choiceCold'>
+          Cold
+        </label>
+      </div>
+    </ModalWithForm>
   );
 };
 
