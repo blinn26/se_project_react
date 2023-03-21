65fb68ecf54653594e18771227f65a4b42b5934d
createdbybenlinn <111088084+blinn26@users.noreply.github.com>
2023-03-21 14:16:18 -0700

    add the import

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SideBar from '../SideBar/SideBar';
 import ClothesSection from '../ClothesSection/ClothesSection';
+import './Profile.css';
 
 function Profile({ cards, handleAddClick, onCardClick }) {
   return (
