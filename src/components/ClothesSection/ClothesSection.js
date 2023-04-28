02d59b2e9224940e1ccdd0e67ff5c01d6754f8ab
createdbybenlinn <benlinn26@gmail.com>
2023-04-28 09:06:11 -0700

    fixing changes

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ItemCard from '../ItemCard/ItemCard';
 import './ClothesSection.css';
 
-const ClothesSection = ({ cards, handleAddClick, onCardClick, currentUser }) => {
+const ClothesSection = ({ cards, handleAddClick, onCardClick }) => {
   return (
     <div className='clothes-section'>
       <div className='clothes-section-header'>
-        <h2 className='clothes-section-title'>{currentUser ? `${currentUser.name}'s Items` : 'Your Items'}</h2>
+        <h2 className='clothes-section-title'>Your Items</h2>
         <button className='clothes-section-add-button' onClick={handleAddClick} type='button'>
           + Add new
         </button>
