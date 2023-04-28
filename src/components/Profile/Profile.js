3a1dbd5d9de73a9d29147c5974f7575a9441c54f
createdbybenlinn <benlinn26@gmail.com>
2023-04-28 09:05:31 -0700

    save reverts

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,18 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import SideBar from '../SideBar/SideBar';
 import ClothesSection from '../ClothesSection/ClothesSection';
 import './Profile.css';
-import CurrentUserContext from '../../context/currentUserContext';
 
 function Profile({ cards, handleAddClick, onCardClick }) {
-  const currentUser = useContext(CurrentUserContext);
-
   return (
     <div className='profile'>
       <div className='profile-container'>
         <SideBar />
-        {/* Example: Display user's avatar */}
-        {currentUser && <img src={currentUser.avatar} alt={currentUser.name} className='profile-avatar' />}
         <ClothesSection cards={cards} handleAddClick={handleAddClick} onCardClick={onCardClick} />
       </div>
     </div>
