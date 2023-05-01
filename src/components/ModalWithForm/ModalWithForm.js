7854cb529867214821134edf68ffdab79b06c2b1
createdbybenlinn <benlinn26@gmail.com>
2023-05-01 10:55:38 -0700

    save prior to tutor

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -3,15 +3,17 @@ import './ModalWithForm.css';
 
 function ModalWithForm({ title, name, buttonText, onSubmit, children, isOpen, onClose }) {
   return (
-    <div className={`modal ${isOpen ? 'modal_open' : ''}`}>
-      <form className='modal__form' name={name} onSubmit={onSubmit}>
-        <h2 className='modal__title'>{title}</h2>
-        {children}
-        <button className='modal__button-submit' type='submit'>
-          {buttonText || 'Submit'}
-        </button>
-        <button className='modal__button-close' type='button' onClick={onClose}></button>
-      </form>
+    <div className='modal'>
+      <div className={`modal__container ${isOpen ? 'modal__container_open' : ''}`}>
+        <form className='modal__form' name={name} onSubmit={onSubmit}>
+          <h2 className='modal__title'>{title}</h2>
+          {children}
+          <button className='modal__button-submit' type='submit'>
+            {buttonText || 'Submit'}
+          </button>
+          <button className='modal__button-close' type='button' onClick={onClose}></button>
+        </form>
+      </div>
     </div>
   );
 }
