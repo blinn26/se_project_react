ad5d82c0e669ec36575945d8e4af8a92c334131a
createdbybenlinn <benlinn26@gmail.com>
2023-05-02 09:18:41 -0700

    sidebarprofile save

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import SideBar from '../SideBar/SideBar';
 import ClothesSection from '../ClothesSection/ClothesSection';
 import './Profile.css';
 
 function Profile({ cards, handleAddClick, onCardClick, onCardLike }) {
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      history.push('/');
+    }
+  }, [history]);
+
   return (
     <div className='profile'>
       <div className='profile-container'>
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,19 +1,23 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import './SideBar.css';
 import avatarUser from '../../images/Avatar.svg';
 import CurrentUserContext from '../../context/currentUserContext';
 
 function SideBar() {
   const currentUser = useContext(CurrentUserContext);
+  const history = useHistory();
 
-  const handleOpenProfileModal = () => {};
+  const handleEditProfileClick = () => {
+    history.push('/');
+  };
 
   return (
     <div className='side-bar'>
       <div className='side-bar__container'>
         <img src={currentUser?.avatar || avatarUser} alt='avatar' className='side-bar__avatar' />
         <p className='side-bar__username'>{currentUser?.name || 'User'}</p>
-        <button onClick={handleOpenProfileModal} className='side-bar__button'>
+        <button onClick={handleEditProfileClick} className='side-bar__button'>
           Change Profile Data
         </button>
       </div>
