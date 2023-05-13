e742d76be03367a6084d5f2658cbcf39598bc314
createdbybenlinn <benlinn26@gmail.com>
2023-05-13 07:30:00 -0700

    api save front

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://wtwr.crabdance.com' : 'http://localhost:3001';
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.wtwr.crabdance.com' : 'http://localhost:3001';
 
 const Api = {
   request: async (url, options = {}) => {
